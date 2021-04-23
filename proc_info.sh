fd674a2b7672eb1b1f2e15c0b40f0f80b91ae136
Daniel Palacios <danpal@tuta.io>
2021-04-23 16:01:51 -0500

    Integrated the usage() function to my conditional cases.

--- proc_info.sh
+++ proc_info.sh
@@ -14,4 +14,64 @@ function usage(){
   echo "-c                   Show command that started the process"
   echo "-f                   Show opened files by the process"
   echo "-h                   Show this help"
+  exit 0
 }
+
+while test $# -gt 0; do
+  case "$1" in
+    -h) usage ;;
+    -i)
+      shift
+      if test $# -gt 0; then
+        pid=$1
+        who_folder=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $9 }')
+        who_user=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $3 }')
+        triggered_by_cmd=$(tr -d '\0' < /proc/$pid/cmdline)
+        echo "$pid Process"
+        echo "Working directory on $who_folder"
+        echo "Ran by user ${who_user}"
+        echo "Triggered by command ${triggered_by_cmd}"
+      else
+        echo "no process specified"
+        exit 1
+      fi
+      shift
+      ;;
+    -iv2)
+      shift
+      if test $# -gt 0; then
+        pid=$1
+        who_folder=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $9 }')
+        who_user=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $3 }')
+        triggered_by_cmd=$(tr -d '\0' < /proc/$pid/cmdline)
+        echo "$pid Process"
+        echo "Working directory on $who_folder"
+        echo "Ran by user ${who_user}"
+        echo "Triggered by command ${triggered_by_cmd}"
+      else
+        echo "no process specified"
+        exit 1
+      fi
+      shift
+      ;;
+    --info*)
+      if test $# -gt 0; then
+        pid=$(echo $1 | sed -e 's/^[^=]*=//g')
+        who_folder=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $9 }')
+        who_user=$(sudo lsof -w -p ${pid} | grep cwd | awk '{ print $3 }')
+        triggered_by_cmd=$(tr -d '\0' < /proc/$pid/cmdline)
+        echo "$pid Process"
+        echo "Working directory on $who_folder"
+        echo "Ran by user ${who_user}"
+        echo "Triggered by command ${triggered_by_cmd}"
+      else
+        echo "no process specified"
+        exit 1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
